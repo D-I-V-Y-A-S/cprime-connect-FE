f03e10d2dd26584b2857542c563a133ec7d3d619
Sudharshna Lakshmi S <sudharshnalakshmi@gmail.com>
2024-11-12 14:59:08 +0530

    CPC-21-Admin_Dashboard

---
+++ src/Components/AdminDashboardComponent/AdminDashboardComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AdminDashboardComponent = () => {
+  return (
+    <div>
+      <h2>Welcome to CPRIME,</h2>
+      <h2>This is Admin Dashboard Page</h2>
+    </div>
+  )
+}
+
+export default AdminDashboardComponent
---
+++ src/Components/UserDashboardComponent/UserDashboardComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserDashboardComponent = () => {
+  return (
+    <div>
+      <h2>This is User Dashboard Page</h2>
+    </div>
+  )
+}
+
+export default UserDashboardComponent
